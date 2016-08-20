b49fd811de53018d3b62f9317418b92c6af93dc8
xmey95 <xmey95@gmail.com>
2016-08-20 18:31:08 +0200

    Implemented players setters for match

--- bin/setters_ub.php
+++ bin/setters_ub.php
@@ -18,6 +18,23 @@ function decrement_tournament_state($id){
     $db->query("UNLOCK TABLES");
 }
 
+function set_match_id_usr1($id, $id_part){
+    $db=  connect_urbangames();
+    $db->query("LOCK TABLES matches{write}");
+    $id=(int)$id;
+    $query=$db->prepare("UPDATE matches SET id_usr1 = ? WHERE id = ?");
+    $query->execute(array($id, $id_part));
+    $db->query("UNLOCK TABLES");
+}
+
+function set_match_id_usr2($id, $id_part){
+    $db=  connect_urbangames();
+    $db->query("LOCK TABLES matches{write}");
+    $id=(int)$id;
+    $query=$db->prepare("UPDATE matches SET id_usr2 = ? WHERE id = ?");
+    $query->execute(array($id, $id_part));
+    $db->query("UNLOCK TABLES");
+}
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
